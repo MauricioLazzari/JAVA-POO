02c44c39cf86ea2af3811826b2152ba9170342ca
MauricioLazzari <mauricio.j.lazzari@gmail.com>
2022-11-15 23:35:09 -0300

    Curso JAVA POO

--- Aula04 - MétodosEspeciais/src/Caneta.java
+++ Aula04 - MétodosEspeciais/src/Caneta.java
@@ -1,5 +1,33 @@
 public class Caneta {
 
+    public String modelo;
+    private float ponta;
+
+
+    public Caneta(){
+
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public float getPonta(){
+        return this.ponta;
+    }
+
+    public void setPonta(float ponta){
+        this.ponta = ponta;
+    }
+
+    public void status(){
+        System.out.println("Modelo da caneta: " + this.modelo);
+        System.out.println("Ponta da caneta: " + this.ponta);
+    }
 
 
 }
