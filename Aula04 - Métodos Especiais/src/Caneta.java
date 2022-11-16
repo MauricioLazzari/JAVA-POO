4e008930a637f028984c23b479c29d588b8c725e
MauricioLazzari <mauricio.j.lazzari@gmail.com>
2022-11-15 23:57:43 -0300

    Curso JAVA POO

---
+++ Aula04 - Métodos Especiais/src/Caneta.java
@@ -0,0 +1,51 @@
+public class Caneta {
+
+    private String modelo;
+    private String cor;
+    private float ponta;
+    private boolean tampada;
+
+    public Caneta() {
+        this.tampada = true;
+        this.cor = "Azul";
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+
+    public void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Tampada? " + this.tampada);
+    }
+}
